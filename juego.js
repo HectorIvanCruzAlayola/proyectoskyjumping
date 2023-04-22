a03b41189762249c4fee89312f7287b7389b7e14
HectorIvanCruzAlayola <hector.calayola@prepafrayangelico.edu.mx>
2023-04-21 20:04:29 -0600

    Segundo Parcial - Primer Avance - Semana 1

--- juego.js
+++ juego.js
@@ -1,147 +1,209 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1420 
+canvas.width = 1420
 canvas.height = 750
 
 const scaledCanvas = {
-    width: canvas.width / 2,
-    height: canvas.height / 2,
+  width: canvas.width / 4.8,
+  height: canvas.height / 4.8,
 }
 
 const floorCollisions2D = []
 for (let i = 0; i < floorCollisions.length; i += 65) {
-    floorCollisions2D.push(floorCollisions.slice(i, i + 65))
+  floorCollisions2D.push(floorCollisions.slice(i, i + 65))
 }
 
 const collisionBlocks = []
 floorCollisions2D.forEach((row, y) => {
-    row.forEach((symbol, x) => {
-        console.log(symbol)
-        if(symbol === 271 || symbol === 280 || symbol === 279) {
-            collisionBlocks.push(new CollisionBlock({position: {
-                x: x * 16,
-                y: y * 16,
-            },
+  row.forEach((symbol, x) => {
+    if (symbol === 271 || symbol === 280 || symbol === 279) {
+      collisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
         })
-        )
-        }
-    })
+      )
+    }
+  })
 })
 
 const platformCollisions2D = []
 for (let i = 0; i < platformCollisions.length; i += 65) {
-    platformCollisions2D.push(platformCollisions.slice(i, i + 65))
+  platformCollisions2D.push(platformCollisions.slice(i, i + 65))
 }
-
 
 const platformCollisionBlocks = []
 platformCollisions2D.forEach((row, y) => {
-    row.forEach((symbol, x) => {
-        console.log(symbol)
-        if(symbol === 279) {
-            platformCollisionBlocks.push(new CollisionBlock({position: {
-                x: x * 16,
-                y: y * 16,
-            },
+  row.forEach((symbol, x) => {
+    if (symbol === 279) {
+      platformCollisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
         })
-        )
-        }
-    })
+      )
+    }
+  })
 })
 
 const gravity = 1
 
-
 const player = new Player({
-    position: {
-        x: 207,
-        y: 350,
+  position: {
+    x: 175,
+    y: 350,
+  },
+  collisionBlocks,
+  platformCollisionBlocks,
+  imageSrc: './img/personaje/Idle.png',
+  frameRate: 8,
+  animations: {
+    Idle: {
+      imageSrc: './img/personaje/Idle.png',
+      frameRate: 8,
+      frameBuffer: 5,
     },
-    collisionBlocks, 
-    imageSrc: './img/personaje/Idle.png',
-    frameRate: 10,
+    Run: {
+      imageSrc: './img/personaje/Run.png',
+      frameRate: 8,
+      frameBuffer: 5,
+    },
+    Jump: {
+      imageSrc: './img/personaje/Jump.png',
+      frameRate: 2,
+      frameBuffer: 3,
+    },
+    Fall: {
+      imageSrc: './img/personaje/Fall.png',
+      frameRate: 2,
+      frameBuffer: 3,
+    },
+    FallLeft: {
+      imageSrc: './img/personaje/FallLeft.png',
+      frameRate: 2,
+      frameBuffer: 3,
+    },
+    RunLeft: {
+      imageSrc: './img/personaje/RunLeft.png',
+      frameRate: 8,
+      frameBuffer: 5,
+    },
+    IdleLeft: {
+      imageSrc: './img/personaje/IdleLeft.png',
+      frameRate: 8,
+      frameBuffer: 3,
+    },
+    JumpLeft: {
+      imageSrc: './img/personaje/JumpLeft.png',
+      frameRate: 2,
+      frameBuffer: 3,
+    },
+  },
 })
 
 const keys = {
-    d: {
-        pressed: false
-    },
-    a: {
-        pressed: false
-    },
-    w: {
-        pressed: false
-    }
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
 }
 
-let lastKey
-
 const background = new Sprite({
-    position: {
-        x: 0,
-        y: 0,
-    },
-    imageSrc: 'img/Fondo.jpeg',
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: 'img/Fondo.jpeg',
 })
 
-function animate () {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'white'
-    c.fillRect(0, 0, canvas.width, canvas.height)
+const backgroundImageHeight = 436
 
-    c.save()
-    // c.scale(3.5, 3.5)
-    // c.translate(0, -background.image.height + scaledCanvas.height)
-    background.update()
-    collisionBlocks.forEach((collisionBlock) => {
-        collisionBlock.update()
-    })
+const camera = {
+  position: {
+    x: 0,
+    y: -backgroundImageHeight + scaledCanvas.height,
+  },
+}
 
-    platformCollisionBlocks.forEach((block) => {
-        block.update()
-    })
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, canvas.width, canvas.height)
 
-    player.update()
+  c.save()
+  c.scale(2.1, 2.1)
+  c.translate(camera.position.x, camera.position.y)
+  background.update()
+  // collisionBlocks.forEach((collisionBlock) => {
+  //   collisionBlock.update()
+  // })
 
-    player.velocity.x = 0
-    if(keys.d.pressed && lastKey === 'd') player.velocity.x = 2
-    else if (keys.a.pressed && lastKey === 'a') player.velocity.x = -2
+  // platformCollisionBlocks.forEach((block) => {
+  //   block.update()
+  // })
 
-    c.restore()
+  player.checkForHorizontalCanvasCollision()
+  player.update()
+
+  player.velocity.x = 0
+  if (keys.d.pressed) {
+    player.switchSprite('Run')
+    player.velocity.x = 2
+    player.lastDirection = 'right'
+    player.shouldPanCameraToTheLeft({ canvas, camera })
+  } else if (keys.a.pressed) {
+    player.switchSprite('RunLeft')
+    player.velocity.x = -2
+    player.lastDirection = 'left'
+    player.shouldPanCameraToTheRight({ canvas, camera })
+  } else if (player.velocity.y === 0) {
+    if (player.lastDirection === 'right') player.switchSprite('Idle')
+    else player.switchSprite('IdleLeft')
+  }
+
+  if (player.velocity.y < 0) {
+    player.shouldPanCameraDown({ camera, canvas })
+    if (player.lastDirection === 'right') player.switchSprite('Jump')
+    else player.switchSprite('JumpLeft')
+  } else if (player.velocity.y > 0) {
+    player.shouldPanCameraUp({ camera, canvas })
+    if (player.lastDirection === 'right') player.switchSprite('Fall')
+    else player.switchSprite('FallLeft')
+  }
+
+  c.restore()
 }
 
 animate()
 
-window.addEventListener('keydown', (event) =>{
-    switch(event.key) {
-        case 'd':
-        keys.d.pressed = true
-        lastKey = 'd'
-        break
-        case 'a':
-        keys.a.pressed = true
-        lastKey = 'a'
-        break
-        case 'w':
-        if (player.velocity.y === 0) keys.w.pressed = player.velocity.y = -12
-        break
-    }
-    console.log(event.key)
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true
+      break
+    case 'a':
+      keys.a.pressed = true
+      break
+    case 'w':
+      if (player.velocity.y === 0) keys.w.pressed = player.velocity.y = -14
+      break
+  }
 })
 
-window.addEventListener('keyup', (event) =>{
-    switch(event.key) {
-        case 'd':
-        keys.d.pressed = false
-        break
-        case 'a':
-        keys.a.pressed = false
-        break
-        case 'w':
-        keys.w.pressed = false
-        break
-    }
-    console.log(event.key)
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+  }
 })
-
