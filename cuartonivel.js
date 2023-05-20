fc79168a6f84025109cdbde89c12da6cdab9867a
HectorIvanCruzAlayola <hector.calayola@prepafrayangelico.edu.mx>
2023-05-19 23:17:10 -0600

    .

---
+++ cuartonivel.js
@@ -0,0 +1,12 @@
+const block_ids = { //if not any of these, then "suelo" // add strings to the collisionblocks
+    "puerta":  3,
+    "pinchos": 1,
+  }
+  
+  const scaling = 3.5 // modify per level xd 
+  const player_coords = [10, 450]
+  const background_image_height = 436
+  
+  
+  const nivel = new Level(3, block_ids, scaling, player_coords, background_image_height) // part constructor variable replace
+  
---
+++ quintonivel.js
@@ -0,0 +1,52 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+const startBtn = document.getElementById('startBtn')
+
+
+canvas.width = 1420
+canvas.height = 750
+
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: 'img/interfaz2.jpg',
+})
+
+const backgroundImageHeight = 436
+
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {     
+      case ' ':
+            window.location.href = "index.html"
+        break;
+    }
+  });
+
+
+function animate() {
+
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  c.save()
+
+  background.update()
+  
+  c.restore()  
+
+  if (Math.floor(Date.now() / 650) % 2 === 0) { // Cambio cada 500 milisegundos
+    startBtn.style.visibility = 'hidden' // Oculta el texto
+  } else {
+    startBtn.style.visibility = 'visible' // Muestra el texto
+  }
+
+}
+
+
+animate()
+
+
